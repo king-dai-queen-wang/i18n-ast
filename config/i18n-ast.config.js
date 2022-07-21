6a320a85f4e68abf5e4f63ffa21aec034a03bff1
代维维 <1006889428@qq.com>
2022-07-21 15:27:26 +0800

    add

--- config/i18n-ast.config.js
+++ config/i18n-ast.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: "./example",
   output: "./example/my-app/src/locales/",
-  exclude: ["**/node_modules/**/*", "./example/my-app/src/components/MultiLanguage.jsx"],
+  exclude: ["**/node_modules/**/*", "**/components/MultiLanguage.jsx"],
   randomFuc: (filePath) => `${filePath.split('/').pop()}-${Math.random().toString(36).substr(2)}`,
   locales: 'zh_CN,pt_PT,en_US'
 }
