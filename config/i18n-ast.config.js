279b86d85c632808cbcc7caff420d7ff5cb2a614
代维维 <1006889428@qq.com>
2022-07-21 15:02:26 +0800

    add

--- config/i18n-ast.config.js
+++ config/i18n-ast.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   entry: "./example",
   output: "./example/my-app/src/locales/",
-  exclude: ["**/node_modules/**/*"],
+  exclude: ["**/node_modules/**/*", "./example/my-app/src/components/MultiLanguage.jsx"],
   randomFuc: (filePath) => `${filePath.split('/').pop()}-${Math.random().toString(36).substr(2)}`,
+  locales: 'zh_CN,pt_PT,en_US'
 }
