18e7d3f3f137f0e46e84368be3df5aefd7ed2713
WDAI13 <wdai13@ford.com>
2023-11-08 13:26:38 +0800

    完成首页的变动

--- i18n-ast.config.js
+++ i18n-ast.config.js
@@ -1,22 +1,10 @@
 module.exports = {
   entry: [
-    // '/Users/david/Desktop/test_projects/my-app/src/'
-    // "example/"
-    // "/Users/david/Desktop/projects/mfe-user-fund/shared/"
-    "/Users/david/Desktop/projects/mfe-user-account/shared/"
-  ], 
+      'C:\\my-study\\my-react-test\\src\\'
+  ],
   output: "./u",
-  exclude: ['**/node_modules/**/*', 'resources/**/*'],
+  exclude: ['**/node_modules/**/*', 'resources/**/*', 'assets/**/*'],
   locales: 'zh_CN,en_US,zh_HK',
   excelName: 'collect.xlsx',
-  autoTranslate: {
-    enable: false,
-    name: "BaiduTranslate",
-    maxLimit: 100, // 新的词条 如果超出该值就不翻译了
-    i18nMapping: {
-      'zh_CN': 'zh',
-      'zh_HK': 'cht',
-      'en_US': 'en',
-    },
-  }
+
 }
